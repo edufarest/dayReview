62eec33c82217f10fe9a017121177da727b3f348
Eduardo Fares <edufarest@gmail.com>
2019-11-23 23:34:16 -0500

    Add React

--- app.js
+++ app.js
@@ -20,5 +20,12 @@ app.use(express.urlencoded({extended: false}));
 
 app.use('/entry', entryRouter)
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client'))
+})
+
 module.exports = app;
 
